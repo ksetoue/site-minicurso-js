414c8ef5785873461df84d831e4a810abc6af5ae
Karoline Setoue <karoline.kimiko@gmail.com>
2016-01-21 22:21:42 -0200

    Alterações no layout

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
 // BIBLIOTECA
 
-var resetCat = function(){
-    document.querySelector("#resultImage").setAttribute("style", 'left: 0px');
-}
 var executeUserCode = function(){
     var idBase = this.getAttribute("id").split("Submit")[0];
     var codigo = $("#"+idBase).val();
@@ -22,11 +19,40 @@ $(document).ready(function(){
     $("#userCode").val(
         '$("#resultImage").animate(\n'+
         '   {\n'+
-        '       left: "700px"\n'+		
+        '       left: "550px"\n'+		
         '   },\n'+
-        '   1000,\n'+
-        '   "linear",\n'+
-        '   resetCat\n'+
+        '   1000\n'+
         ');'         
     );             
 })
+
+// var resetCat = function(){
+//     document.querySelector("#resultImage").setAttribute("style", 'left: 0px');
+// }
+// var executeUserCode = function(){
+//     var idBase = this.getAttribute("id").split("Submit")[0];
+//     var codigo = $("#"+idBase).val();
+//     var containerResultado = $("#section4");
+//     $("body").scrollTop(containerResultado.offset().top);
+//     try{
+//         (new Function(codigo))();
+//     }catch(e){
+//         console.error(e);
+//     }
+// }
+
+
+// // INICIALIZAÇÃO
+// $(document).ready(function(){
+//     $("#userCodeSubmit").click(executeUserCode);
+//     $("#userCode").val(
+//         '$("#resultImage").animate(\n'+
+//         '   {\n'+
+//         '       left: "700px"\n'+		
+//         '   },\n'+
+//         '   1000,\n'+
+//         '   "linear",\n'+
+//         '   resetCat\n'+
+//         ');'         
+//     );             
+// })
